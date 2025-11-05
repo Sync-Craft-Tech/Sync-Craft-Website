62afd9c0952045c7a7f2e74932e78e0e28cb3d55
IncredibleZuess <carlo.barnardo@gmail.com>
2025-11-05 14:50:26 +0200

    Quick fix for linting errors

--- src/components/custom/Navbar.components.tsx
+++ src/components/custom/Navbar.components.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu';
 
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
