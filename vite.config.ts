180722fa2d477207dd2d730c484ec4cdbad01d2c
IncredibleZuess <carlo.barnardo@gmail.com>
2025-11-06 16:00:53 +0200

    Type Issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  let baseURL: String = ''
+  //const env = loadEnv(mode, process.cwd(), '') TODO include env variables with the site
+  let baseURL = ''
   if (mode == "development") {
     baseURL = "/Sync-Craft-Website/"
   } else if (mode == "production"){
