29fe959c783109e98c92883e7426a1f7557389c6
IncredibleZuess <carlo.barnardo@gmail.com>
2025-11-06 15:08:13 +0200

    Change base to actual endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/Sync-Craft-Website/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
